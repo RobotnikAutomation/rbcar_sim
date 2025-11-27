11b74f0015c541cb989a404ba9a04db830ca9012
José Antonio López Palenzuela <jalopez@robotnik.es>
2025-11-27 17:40:43 +0100

    Rebuild error catching.

--- rbcar_docker/build.sh
+++ rbcar_docker/build.sh
@@ -8,6 +8,10 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 SCRIPT_NAME="$(basename "$0")"
 
 # Download repository (will be mounted on the docker container).
+if [[ -d $SCRIPT_DIR/catkin_ws/src  ]]; then
+    echo "ERROR: $SCRIPT_DIR/catkin_ws/src alreay exist. Delete it to be able to build again. All unsaved changes will be lost."
+    exit 1
+fi
 mkdir $SCRIPT_DIR/catkin_ws/src -p
 vcs import $SCRIPT_DIR/catkin_ws/src --input $SCRIPT_DIR/../rbcar_sim.rosinstall
 vcs pull $SCRIPT_DIR/catkin_ws/src
